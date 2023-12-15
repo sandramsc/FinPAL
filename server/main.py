db4933fa051d5ab29c20355d759778f3864f5020
vikyw89 <vikyw89@gmail.com>
2023-12-16 00:36:34 +0800

    chore: added cors middleware

--- server/main.py
+++ server/main.py
@@ -5,8 +5,25 @@ load_dotenv()
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
