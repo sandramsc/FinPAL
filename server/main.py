6f7779e7c1b768f27b49d9c8b79339b67b28e62f
vikyw89 <vikyw89@gmail.com>
2023-12-16 10:21:28 +0800

    chore: setup webhook on init

--- server/main.py
+++ server/main.py
@@ -1,20 +1,35 @@
 from dotenv import load_dotenv
+from fastapi.responses import RedirectResponse
+import httpx
 
 load_dotenv()
-
+import os
 from typing import Union
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SERVER_URL = os.getenv("SERVER_URL")
 
+if not TELEGRAM_BOT_TOKEN:
+    raise Exception("TELEGRAM_BOT_TOKEN is not set")
+
+if not SERVER_URL:
+    raise Exception("SERVER_URL is not set")
+
+# init webhook
+httpx.get(
+    f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={SERVER_URL}/telegram_webhook"
+)
+
+app = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:8000",
 ]
 
 app.add_middleware(
@@ -28,7 +43,12 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return RedirectResponse("/docs")
+
+
+@app.get("/telegram_webhook")
+async def telegram_webhook():
+    return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
