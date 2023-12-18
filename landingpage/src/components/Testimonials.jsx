bbf3e632e980cd2316916e4de937555bf6b3b717
Sandra Ashipala <sandraashipala@gmail.com>
2023-12-18 13:46:15 +0200

    Added a features title to the service card section

--- landingpage/src/components/Testimonials.jsx
+++ landingpage/src/components/Testimonials.jsx
@@ -1,9 +1,16 @@
 import { feedback } from "../constants";
-import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
+import styles, { layout } from "../style";
 
 const Testimonials = () => (
+
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="my-6">
+      <h2 className={styles.heading2}>
+        Features
+      </h2>
+    </div>
+
     <div className="absolute z-[0] w-[30%] h-[30%] -right-[25%] rounded-full blue__gradient bottom-40" />
 
     <div className="flex flex-wrap sm:justify-center justify-center w-full feedback-container relative z-[1]">
