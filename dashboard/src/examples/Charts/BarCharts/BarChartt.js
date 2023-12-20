43c900db28be52aa7bcb7da7f048b006de877106
vikyw89 <vikyw89@gmail.com>
2023-12-20 22:52:59 +0800

    chore: chart displaying income expense

--- dashboard/src/examples/Charts/BarCharts/BarChartt.js
+++ dashboard/src/examples/Charts/BarCharts/BarChartt.js
@@ -16,44 +16,103 @@ ChartJS.register(
   LinearScale,
 );
 
+const baseUrl = "https://finpal-alpha.23.94.26.231.sslip.io/transactions";
+const LABELS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 const BarChart = () => {
   const [chart, setChart] = useState({})
-  var baseUrl = "https://api.coinranking.com/v2/coins/?limit=10";
-  var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  var apiKey = "coinrankinga0d622c9c87f41f10b19b797ba4f932fa68aef3daff45766";
 
+  // var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  // var apiKey = "coinrankinga0d622c9c87f41f10b19b797ba4f932fa68aef3daff45766";
 
   useEffect(() => {
-    const fetchCoins = async () => {
-      await fetch(`${proxyUrl}${baseUrl}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-access-token': `${apiKey}`,
-          'Access-Control-Allow-Origin': "*"
-        }
-      })
-        .then((response) => {
-          if (response.ok) {
-            response.json().then((json) => {
-              console.log(json.data);
-              setChart(json.data)
-            });
-          }
-        }).catch((error) => {
-          console.log(error);
-        });
-    };
-    fetchCoins()
-  }, [baseUrl, proxyUrl, apiKey])
+    const fetchTransactions = async ({ startDate, endDate, userId }) => {
+
+      const response = await fetch(`${baseUrl}/?start_date=${startDate}&end_date=${endDate}&user_id=${userId}`);
+      const data = await response.json();
+
+      let expenseDataset = new Array(12).fill(0)
+      let incomeDataset = new Array(12).fill(0)
+      for (const transaction of data.transactions) {
+        const amountIn = Number(transaction.amountIn)
+        const amountOut = Number(transaction.amountOut)
+        const category = transaction.category
+        const createdAt = transaction.createdAt
+        const currency = transaction.currency
+        const description = transaction.description
+        const id = transaction.id
+        const sourceOrPayee = transaction.sourceOrPayee
+        const transactionDate = transaction.transactionDate
+        const updatedAt = transaction.updatedAt
+        const userId = transaction.userId
+
+        // remove 0 at the left
+        const labelIndex = Number(transactionDate.slice(4, 6)).toString()
+        console.log("🚀 ~ file: BarChartt.js:41 ~ fetchTransactions ~ labelIndex:", labelIndex)
+
+        incomeDataset[labelIndex] += amountIn
+        expenseDataset[labelIndex] += amountOut
+        setChart({
+          labels: LABELS,
+          incomeDataset: incomeDataset,
+          expenseDataset: expenseDataset
+        })
+      }
+    }
+    fetchTransactions({ startDate: "20230101", endDate: "20231212", userId: "test" });
+  },[])
+
+  // useEffect(() => {
+  //   const fetchCoins = async () => {
+  //     await fetch(`${proxyUrl}${baseUrl}`, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-access-token': `${apiKey}`,
+  //         'Access-Control-Allow-Origin': "*"
+  //       }
+  //     })
+  //       .then((response) => {
+  //         if (response.ok) {
+  //           response.json().then((json) => {
+  //             console.log(json.data);
+  //             setChart(json.data)
+  //           });
+  //         }
+  //       }).catch((error) => {
+  //         console.log(error);
+  //       });
+  //   };
+  //   fetchCoins()
+  // }, [baseUrl, proxyUrl, apiKey])
 
   console.log("chart", chart);
-  var data = {
-    labels: chart?.coins?.map(x => x.name),
+  const data = {
+    labels: LABELS,
     datasets: [{
-      label: `${chart?.coins?.length} Coins Available`,
-      data: chart?.coins?.map(x => x.price),
+      label: chart.labels,
+      data: chart.incomeDataset,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    },{
+      label: chart.labels,
+      data: chart.expenseDataset,
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -74,7 +133,7 @@ const BarChart = () => {
     }]
   };
 
-  var options = {
+  const options = {
     maintainAspectRatio: false,
     scales: {
     },
@@ -91,7 +150,6 @@ const BarChart = () => {
         data={data}
         height={400}
         options={options}
-
       />
     </div>
   )
