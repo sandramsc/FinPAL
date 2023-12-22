d65db3c71c43db8ca66efe3ab93cc5d87696efbf
Sandra Ashipala <sandraashipala@gmail.com>
2023-12-22 08:30:55 +0200

    Removed business section from landing page

--- landingpage/src/components/index.js
+++ landingpage/src/components/index.js
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import CardDeal from "./CardDeal";
-import Business from "./Business";
 import Clients from "./Clients";
 import CTA from "./CTA";
 import Footer from "./Footer";
@@ -10,7 +9,6 @@ import Hero from "./Hero";
 export {
   Navbar,
   CardDeal,
-  Business,
   Clients,
   CTA,
   Footer,
