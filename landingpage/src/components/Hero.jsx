7a53dfeced24f693c6b438033ceebd6927ae6e61
Sandra Ashipala <sandraashipala@gmail.com>
2023-12-22 12:01:37 +0200

    Adjusted signin signup link

--- landingpage/src/components/Hero.jsx
+++ landingpage/src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         </p>
       </div>
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={robot} alt="finpaldemo" className="w-[100%] h-[100%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
