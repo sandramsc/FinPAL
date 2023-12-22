2f40b9c6d129f62a44c64ade7008a2e7a633d8d5
Sandra Ashipala <sandraashipala@gmail.com>
2023-12-22 14:40:26 +0200

    Added QR code to landing page

---
+++ landingpage/src/components/Scan_Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    SCAN ME
+  </button>
+);
+
+export default Button;
